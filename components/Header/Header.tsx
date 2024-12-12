26110f690dbdfb6e89986a063ca4f38cab1769c3
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-12-13 00:48:11 +0900

    попытка концепт сайта 1

---
+++ components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+import style from "./Header.module.scss"
+
+export default function Header() {
+  return (
+    <div className={style.Header}>
+        <div className={style.Header__container}>
+            <Link href='#' className={style.logo}>Logo</Link>
+            <ul>
+                <Link href='#' className="NavItem">Об проекте</Link>
+                <Link href='#' className="NavItem">Тест</Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
--- components/Main/MainH.tsx
+++ components/Main/MainH.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../Header/Header'
+import style from './MainH.module.scss'
 
 export default function MainH() {
   return (
-    <div>MainH</div>
+    <div className={style.MainH}>
+        <Header />
+        asdasd
+    </div>
   )
 }
